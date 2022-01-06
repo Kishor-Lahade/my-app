6139bbebd3d04b6fd0462a39a8f6340427545c34
Kishor Lahade <lahadekishor@gmail.com>
2022-01-07 00:51:32 +0530

    b1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useState } from "react";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Components />
     </div>
   );
 }
 
-export default App;
+function Components() {
+  const [id, setid] = useState("");
+  const [username, setusername] = useState("");
+  const [password, setpassword] = useState("");
+  const [list, setList] = useState([]);
+
+  const addid = (e) => {
+    setid(e.target.value);
+  };
+
+  const addInput = (e) => {
+    setusername(e.target.value);
+  };
+
+  const addPassword = (e) => {
+    setpassword(e.target.value);
+  };
+
+  const submit = () => {
+    const url = "http://localhost:5000/hello";
+    const data = {
+      id: id,
+      username: username,
+      password: password,
+    };
+
+    axios.post(url, data);
+    const newList = [data, ...list];
+    setList(newList);
+    setid("");
+    setusername("");
+    setpassword("");
+  };
+
+  const getUser1 = async () => {
+    const url = "http://localhost:5000/index";
+    const result = await axios.get(url);
+
+    const list = result.data;
+    const newList = [...list];
+    setList(newList);
+  };
+
+  return (
+    <div>
+      <div>
+        <input type="text" value={id} onChange={addid} />
+      </div>
+      <div>
+        <input type="text" value={username} onChange={addInput} />
+      </div>
+      <div>
+        <input type="text" value={password} onChange={addPassword} />
+      </div>
+      <div>
+        <button className="bg-success" onClick={submit}>
+          click
+        </button>
+      </div>
+      <div>
+        {list.map((item, index) => (
+          <div key={index} className="alert alert-secondary fs-4">
+            {item.id}
+            {item.username} {item.password}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
